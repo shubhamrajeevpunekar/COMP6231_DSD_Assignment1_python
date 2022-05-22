55c6d58e88e6c415362608638a8bcc125d215f55
FelipeJorge805 <feliperkj805@gmail.com>
2022-05-21 23:12:25 -0400

    added a case for every input which maps to a dictionary function call and returns a message to the user

--- ERAP_TCP.py
+++ ERAP_TCP.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 
+from Dictionary import Dictionary
+
 localIP = sys.argv[1]
 localPort = int(sys.argv[2])
 localAddress = (localIP, localPort)
@@ -8,22 +10,56 @@ localAddress = (localIP, localPort)
 TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 TCPSocket.bind(localAddress)
-TCPSocket.listen(65536)
+TCPSocket.listen(1024)
 
 conn, address = TCPSocket.accept()
-str = ''
-while(True):
+conn.sendall("Welcome to Distributed Repositories\033[2;0H".encode())
+
+dic = Dictionary()
+line = ''
+
+while (True):
     data = conn.recv(1024).decode()
-    #print(data)
-    str += data
-    if str.__contains__('\n'):
-        print(str)
-        print('newline!')
-        #conn.sendall(str.encode())
+    # print(data)
+    line += data
+    if line.__contains__('\n'):
+        # print(line)
+        # print('newline!')
+        if line == '\n':
+            continue
+        command = line.split(' ')
+        message = ''
+        if command[0].upper() == 'GET':
+            message = dic.get(command[1])
+        if command[0].upper() == 'GETALL':
+            message = dic.get(command[1])
+        if command[0].upper() == 'SET':
+            message = dic.set(command[1], command[2])
+        if command[0].upper() == 'DELETE':
+            message = dic.delete(command[1])
+        if command[0].upper() == 'LIST':
+            message = dic.list()
+        if command[0].upper() == 'DELETE':
+            message = dic.delete(command[1])
+        if command[0].upper() == 'ADD':
+            message = dic.add(command[1], command[2])
+        if command[0].upper() == 'MAX':
+            message = dic.max(command[1])
+        if command[0].upper() == 'MIN':
+            message = dic.min(command[1])
+        if command[0].upper() == 'AVG':
+            message = dic.avg(command[1])
+        if command[0].upper() == 'SUM':
+            message = dic.sum(command[1])
+        if command[0].upper() == 'RESET':
+            message = dic.reset()
         # send this data to the directory: DIRECTORY.add(data)
-        str = ''
+        if message.__contains__('Error'):
+            conn.sendall(message.encode())
+        else:
+            conn.sendall(('OK ' + message).encode())
+        line = ''
     if not data:
         break
 
-    #conn.sendall(data)
-
+    # conn.sendall(data)
